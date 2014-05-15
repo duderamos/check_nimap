5ea61407fc3536821cf9edd042e748df9248b67d
duderamos <freedom.trinity@gmail.com>
2014-05-15 15:29:35 -0300

    Implemented with Nagios Guidelines (https://nagios-plugins.org/doc/guidelines.html)
    Fixed Performance data, removing minimal, warning, critical and maximum values.
    
    Included number of users logged in performance data.

--- check_nimap.py
+++ check_nimap.py
@@ -1,15 +1,48 @@
-#!/bin/env python
+#!/usr/bin/env python
 
-import os, sys, re
+import getopt, sys
+import os, re
 
-a = 'doveadm -f flow who'
-b = os.popen(a, 'r', 10240)
-c = 0
-t = 0
-for line in b:
-        m = re.search(r'#=(\d+)', line, re.I)
-        if m:
-                c = c + 1
-                t = t + int(m.group(1))
-print "# IMAP OK - ({1}/{0}) (users/total)|nimap={0};0;0;0".format(t,c)
-sys.exit(0)
+def version():
+        print 'check_nimap.py 0.1'
+        print 'Eduardo Ramos <eduardo@freedominterface.org>'
+        sys.exit(3)
+
+def usage():
+        print 'check_nimap.py [-v]'
+        sys.exit(3)
+
+def main():
+        minimal = 0
+        critical = 0
+        warning = 0
+        cmd = 'doveadm -f flow who'
+        count_per_login = 0
+        count_total = 0
+
+        try:
+                opts, args = getopt.getopt(sys.argv[1:], 'vh')
+        except getopt.GetoptError as err:
+                print str(err)
+                usage()
+                sys.exit(3)
+                
+        for o, a in opts:
+                if o == '-v':
+                        version()
+                else:
+                        usage()
+                        assert False, 'unhandled option'
+
+        pd = os.popen(cmd, 'r', 10240)
+        for line in pd:
+                m = re.search(r'#=(\d+)', line, rc.I)
+                if m:
+                        count_per_login = count_per_login + 1
+                        count_total = count_total + int(m.group(1))
+
+        print "# IMAP OK - ({0}/{1}) (users/total)|nusers={0} nimap={1}".format(count_per_login,count_total)
+        sys.exit(0)
+
+if __name__ == '__main__':
+        main()
